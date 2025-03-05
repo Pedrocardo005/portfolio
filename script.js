e2ebad546cbba1ce1f6160f82544c40ade3f694e
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-03-05 15:07:29 -0300

    :sparkles: feat: Torna form contato funcionavel

--- script.js
+++ script.js
@@ -26,3 +26,43 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+const form = document.getElementById('mail-form');
+
+const successMessage = document.getElementById('success-message');
+const errorMessage = document.getElementById('error-message');
+
+function showMessage(element) {
+    element.classList.remove('hidden');
+    element.style.display = 'block';
+
+    setTimeout(() => {
+        element.style.display = 'none';
+    }, 10000); // A mensagem some após 3 segundos
+}
+
+form.addEventListener('submit', async function (ev) {
+    ev.preventDefault();
+    const formData = new FormData(ev.target);
+    const formProps = Object.fromEntries(formData);
+
+    const opcoes = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formProps)
+    };
+
+    fetch("https://60bfbc0597295a0017c43b7e.mockapi.io/argon", opcoes)
+        .then(response => {
+            if (!response.ok) throw new Error('Erro na requisição: ' + response.status);
+            return response.json();
+        })
+        .then(() => {
+            ev.target.reset();
+            showMessage(successMessage);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            showMessage(errorMessage);
+        });
+});
